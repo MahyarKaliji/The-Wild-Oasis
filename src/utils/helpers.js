da175c9171210c168683002a1ee77bfb5e064507
Mahyar Kaliji <mahyar.kaliji@gmail.com>
2025-07-02 15:48:07 +0330

    added booking table

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,7 +3,7 @@ import { differenceInDays } from "date-fns/fp";
 
 // We want to make this function work for both Date objects and strings (which come from Supabase)
 export const subtractDates = (dateStr1, dateStr2) =>
-  differenceInDays(parseISO(String(dateStr1)), parseISO(String(dateStr2)));
+  differenceInDays(parseISO(String(dateStr2)), parseISO(String(dateStr1)));
 
 export const formatDistanceFromNow = (dateStr) =>
   formatDistance(parseISO(dateStr), new Date(), {
